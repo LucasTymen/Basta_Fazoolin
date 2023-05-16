aa67e310366509dd5956259056f9f3b2d3e7c5a4
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-16 10:34:02 +0200

    Basta Fazoolin_step_01

--- script.py
+++ script.py
@@ -10,6 +10,6 @@ class Menu:
 brunch_items = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }
-brunch_menu = Menu('runch', brunch_items,1100, 1600)
+brunch_menu = Menu('Brunch', brunch_items,1100, 1600)
 
-print(brunch_menu)
+print(brunch_menu.name)
